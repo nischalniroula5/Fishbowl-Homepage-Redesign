f8977cdaa652c0d0f854d572480656cc5765ae0c
Nischal Niroula <officialnischalniroula@gmail.com>
2024-01-24 14:18:47 +1100

    Initial Commit

---
+++ Fishbowl/FishbowlApp.swift
@@ -0,0 +1,17 @@
+//
+//  FishbowlApp.swift
+//  Fishbowl
+//
+//  Created by Nischal Niroula on 24/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct FishbowlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
